b726522ac4f4dd5bb45efebcd7e9bec7519cd551
sivaram <53555951+sivarampatamsetti@users.noreply.github.com>
2021-06-26 11:47:30 +0530

    Add files via upload

---
+++ MINI Project (3)/flaskblog/forms.py
@@ -0,0 +1,96 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from wtforms import validators
+from flaskblog.models import User
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('ID Number',
+                           validators=[DataRequired(), validators.Regexp(regex="(s|s)[0-9]{6}",message="Enter College ID Number")])
+    email = StringField('Email',
+                        validators=[DataRequired(), validators.Regexp(regex="(s|S)[0-9]{6}[@](rguktsklm).(ac).(in)",message="Type only the College mail")])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is taken. Please choose a different one.')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), validators.Regexp(regex="(s|S)[0-9]{6}[@](rguktsklm).(ac).(in)",message="Type only the College mail")])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('ID Number',
+                           validators=[DataRequired(),validators.Regexp(regex="(s|s)[0-9]{6}",message="Enter College ID Number")])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('That email is taken. Please choose a different one.')
+
+
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    file=FileField("Upload Files",validators=[FileAllowed(['pdf','ppt','txt','jpg','png'])])
+    submit = SubmitField('Post')
+
+
+class RequestResetForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is None:
+            raise ValidationError('There is no account with that email. You must register first.')
+
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Reset Password')
+
+
+class NotifyForm(FlaskForm):
+    reason=TextAreaField('Reason', validators=[DataRequired()])
+    submit = SubmitField('Send')
+
+
+
+class CommentForm(FlaskForm):
+    comment=TextAreaField('Comment', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+
